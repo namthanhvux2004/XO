3933eb608b5099af7f058da7854551ac7803f018
namthanhvux2004 <138623384+namthanhvux2004@users.noreply.github.com>
2023-07-09 00:51:39 +0700

    Add files via upload

---
+++ XO.js
@@ -0,0 +1,16 @@
+const s=[0, 0, 0, 0, 0, 0, 0, 0, 0];
+const id=["Zone1","Zone2","Zone3","Zone4","Zone5","Zone6","Zone7","Zone8","Zone9"];
+var turn = 0;
+function Move(i)
+    {
+            s[i]++;
+            if (s[i]==1)
+            {
+                turn++;
+                let tmp =  document.getElementById(id[i]);
+                tmp = tmp.getElementsByTagName("p");
+                if (turn%2==0) tmp.innerHTML = 'X';
+                else tmp.innerHTML = 'O';
+                console.log(turn, i);
+            }
+    }
